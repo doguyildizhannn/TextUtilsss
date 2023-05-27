2789c0688337ac0ed3f48a073c985bb88bf3f969
doguyildizhannn <doguyildizhannn@gmail.com>
2023-05-27 17:15:53 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+//import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  /*Link*/
+} from "react-router-dom";
+
+function App() {
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000)
+  }
+
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = '#143564';
+      showAlert("Dark Mode set", "success");
+      document.title = "TextUtilsss - Dark - Home";
+      // setInterval(() =>{
+      //   document.title = "Install TextUtilsss now";
+      // }, 2000)
+    }
+    else {
+      setMode("light");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode set", "success");
+      document.title = "TextUtilsss - Light - Home";
+    }
+  }
+  return (
+    <div className="First">
+      <Router>
+        <Navbar title="TextUtilsss" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm text="Enter some text to analyze" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+
+    const handleOnClickUpper = () => {
+        let upperText = text.toUpperCase()
+        setText(upperText)
+        //console.log(text)
+        props.showAlert("Converted to uppercase!", "success");
+    }
+
+    const handleOnClickLower = () => {
+        let lowerText = text.toLowerCase()
+        setText(lowerText)
+        //console.log(text)
+        props.showAlert("Converted to lowercase!", "success");
+    }
+
+    const handleOnClickClear = () => {
+        let myNewText = '';
+        setText(myNewText)
+        //console.log(text)
+        setWordCount(myNewText.split(' ').filter(i => i !== '').length)
+        setCharCount(myNewText.length)
+        props.showAlert("Text cleared!", "success");
+    }
+
+    const handleRemoveExtra = () => {
+        let myNewText = text.split(/[ ]+/); //regex
+        setText(myNewText.join(" "))
+        //console.log(text)
+        setWordCount(myNewText.filter(i => i !== '').length)
+        setCharCount(myNewText.filter(i => i !== '').join(" ").length)
+        props.showAlert("Extra spaces are removed!", "success");
+    }
+
+    const handleOnChange = (event) => {
+        let myNewText = event.target.value
+        setText(myNewText)
+        //console.log(text)
+        setWordCount(myNewText.split(' ').filter(i => i !== '').length)
+        setCharCount(myNewText.length)
+    }
+
+    const [text, setText] = useState('');
+    const [wordCount, setWordCount] = useState(0);
+    const [charCount, setCharCount] = useState(0);
+
+    return (
+        <div>
+            <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
+                <div className="mb-3">
+                    <h1>{props.text}</h1>
+                    <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='light'?'white':'grey',
+                                                                            color: props.mode==='light'?'black':'white'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleOnClickUpper} >Convert to uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleOnClickLower} >Convert to lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleOnClickClear} >Clear text</button>
+                <button className="btn btn-primary mx-2" onClick={handleRemoveExtra} >Remove extra spaces</button>
+            </div>
+            <div className="container my-2" style={{color: props.mode==='light'?'#0c60da':'white'}}>
+                <h2>Your text summary</h2>
+                <p><b>{wordCount}</b> words and <b>{charCount}</b> characters</p>
+                <p><b>{0.008 * wordCount}</b> minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+}
